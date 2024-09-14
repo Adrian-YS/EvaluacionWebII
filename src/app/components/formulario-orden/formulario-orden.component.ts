e275fd6acdd0b57b8c865bc758bae33bc416cf36
Adrian-YS <dnoxus2003@gmail.com>
2024-09-14 09:57:39 -0600

    Correccion

---
+++ src/app/components/formulario-orden/formulario-orden.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-orden',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-orden.component.html',
+  styleUrl: './formulario-orden.component.css'
+})
+export class FormularioOrdenComponent {
+
+  id: any;
+  mesa: any;
+  
+
+}
